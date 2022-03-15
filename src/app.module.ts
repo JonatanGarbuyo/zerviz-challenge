4ad05f989b58dfeee44354539713431e94e0e758
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2022-03-14 23:24:47 -0300

    add post module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PostsModule } from './posts/posts.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    PostsModule,
+    MongooseModule.forRoot(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
